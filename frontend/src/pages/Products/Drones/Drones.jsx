8d00ab1ba6bbe42bc2d85328b82b0dacccca3062
marcos <yoldimarcos@gmail.com>
2022-09-21 12:55:48 -0300

    drones page finished

--- frontend/src/pages/Products/Drones/Drones.jsx
+++ frontend/src/pages/Products/Drones/Drones.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import Card from '../../../components/Card/Card';
+
+import img1 from'../../../images/drone1.jpg';
+import img2 from'../../../images/drone2.jpg';
+import img3 from'../../../images/drone3.jpg';
 
 const Drones = () =>{
-
     return(
-    <>
-    <h1>Drones</h1>
-    </>
-    )
+        <>
+        <div style={{margin:'auto', display:'flex',justifyContent:'center',flexDirection:'column'}}>
+        <h1>Drones</h1>
+        <div style={{display:'flex', justifyContent:'spaceAround', margin:'auto'}}>
+            <Card img={img1}></Card>
+            <Card img={img2}></Card>
+            <Card img={img3}></Card>
+        </div>
+        </div>
+        </>
+        )
 }
+
 
 export default Drones;
