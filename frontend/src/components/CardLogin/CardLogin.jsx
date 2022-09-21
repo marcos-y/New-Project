5570536c173afb26099bee2590df9bce1ef0855b
marcos <yoldimarcos@gmail.com>
2022-09-21 20:14:37 -0300

    first-login

---
+++ frontend/src/components/CardLogin/CardLogin.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function MultiActionAreaCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            LogIn
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Please, login with your email or username
+          </Typography>
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import CardLogin from '../../components/CardLogin/CardLogin';
 
 const Login = () =>{
 
     return( 
     <>
-    <h1>Login</h1>
+    <div style={{margin:'auto',display:'flex',justifyContent:'center',flexDirection:'column'}}>
+        <h1>Login</h1>
+        <div style={{margin:'auto'}}>
+            <CardLogin></CardLogin>
+        </div> 
+    </div>
     </>
     )
 }
