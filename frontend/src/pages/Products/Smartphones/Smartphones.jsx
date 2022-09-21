a5c04020b1f1e3977da0a4754d25cfed6aca48c7
marcos <yoldimarcos@gmail.com>
2022-09-21 12:43:20 -0300

    smartphones page finished

--- frontend/src/pages/Products/Smartphones/Smartphones.jsx
+++ frontend/src/pages/Products/Smartphones/Smartphones.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Card from '../../../components/Card/Card';
+
+import img1 from'../../../images/smartphone1.jpg';
+import img2 from'../../../images/smartphone2.jpg';
+import img3 from'../../../images/smarthpone3.jpg';
 
 const Smartphones = () =>{
     return(
         <>
+        <div style={{margin:'auto', display:'flex',justifyContent:'center',flexDirection:'column'}}>
         <h1>Smartphones</h1>
+        <div style={{display:'flex', justifyContent:'spaceAround', margin:'auto'}}>
+            <Card img={img1}></Card>
+            <Card img={img2}></Card>
+            <Card img={img3}></Card>
+        </div>
+        </div>
         </>
         )
 }
